62e7aebf3951f39d4de05539a1c1036bf94c9308
kate <katejim@mail.ru>
2015-10-14 01:31:30 +0300

    change match weigth

--- Alignments/src/Main.java
+++ Alignments/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         String first = "ATTAGCTCGTCTCATTGTATGTTCATCACTCCTCCCGACAAAAAGCATGAATCAGTAGTAACACTGCT";
         String second = "TCAGGTAATAAAAAAGCCGGAGCTCCCTTTCCGGCACGTCTCATTGCCGTATCACTTCCTCCCGAAAAAGCATTAATCAGTAAAACCCGACTGCTCAGCGAGAATCGTCGAAAAGGACG";
-        GlobalAlignment globalAlignment = new GlobalAlignment(2, 1, 1);
+        GlobalAlignment globalAlignment = new GlobalAlignment(1, 1, 1);
         Pair<String, String> result = globalAlignment.getPath(first, second);
         System.out.println(result.getKey() + "\n" + result.getValue());
     }
